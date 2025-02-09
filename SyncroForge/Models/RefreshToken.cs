c70d57bbab33cf98c5f5d0d539c038423be18354
mhmdzrkt34 <izaraket48@gmail.com>
2025-02-09 22:06:08 +0200

    SCRUM-8 >> adding models and updating configuration

---
+++ SyncroForge/Models/RefreshToken.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace SyncroForge.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string PublicKey { get; set; }
+        public string Token {  get; set; }
+        public long ExpiryAt { get; set; }
+        public long UpdatedAt { get; set; }
+        public long CreatedAt { get; set; }
+        public bool IsDeleted {  get; set; }
+        public int UserId {  get; set; }
+        public virtual User User { get; set; }
+
+
+        public RefreshToken()
+        {
+            PublicKey = Guid.NewGuid().ToString();
+            CreatedAt = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            UpdatedAt = CreatedAt;
+            ExpiryAt = CreatedAt + 604800 ;
+            
+
+        }
+
+    }
+}
--- SyncroForge/Program.cs
+++ SyncroForge/Program.cs
@@ -17,11 +17,12 @@ namespace SyncroForge
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            Console.WriteLine("env:"+Env.currentEnvironment);
             builder.Configuration
            .SetBasePath(Directory.GetCurrentDirectory())
            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
             builder.Configuration.AddJsonFile($"appsettings.{Env.currentEnvironment}.json", optional: true, reloadOnChange: true);
-            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetSection("connectionString:defaultConnection").Value));
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
             var app = builder.Build();
