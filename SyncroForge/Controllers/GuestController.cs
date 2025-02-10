eb8e8276ed37d03bce512a5725ea8a795cee6c7d
mhmdzrkt34 <izaraket48@gmail.com>
2025-02-10 22:00:41 +0200

    Authorization >> saving

--- SyncroForge/Controllers/GuestController.cs
+++ SyncroForge/Controllers/GuestController.cs
@@ -1,11 +1,38 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SyncroForge.Requests.Guest;
+using SyncroForge.Responses.Guest.RegisterResponse;
+using SyncroForge.Services.Guest;
 
 namespace SyncroForge.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class GuestController : ControllerBase
     {
+        private readonly IGuestService _guestService;
+        public GuestController(IGuestService guestService)
+        {
+            _guestService = guestService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Regiter(RegisterRequest request)
+        {
+
+            RegisterResponse response = await _guestService.Register(request);
+            return StatusCode(response.Status, response);
+
+            
+
+        }
+        public async Task<IActionResult> Login()
+        {
+
+        }
+
+        public async Task<IActionResult> CheckIfEmailAlreadyExist()
+        {
+
+        }
     }
 }
--- SyncroForge/Requests/Guest/RegisterRequest.cs
+++ SyncroForge/Requests/Guest/RegisterRequest.cs
@@ -1,6 +1,19 @@
-﻿namespace SyncroForge.Requests.Guest
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SyncroForge.Requests.Guest
 {
     public class RegisterRequest
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
     }
 }
